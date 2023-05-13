ffe59bc914a38696ec7d52b4c5da00a0da1b7d60
Jeff Parker <jeffbparker@github.com>
2023-05-12 18:57:24 -0700

    update docstrings again

--- non_adaptive_steppers.hpp
+++ non_adaptive_steppers.hpp
@@ -11,10 +11,10 @@ class StepperLieEuler {
     /**
      * @brief Given state y at time t, perform step and update y to time t + dt
      *
-     * @param t - Time at beginning of step
-     * @param dt -timestep
-     * @param rhs_func - Function to compute time derivatives, dy/dt = f(t, y)
-     * @param y - On entry, state at time t.  On exit, state at time t + dt
+     * @param[in] t - Time at beginning of step
+     * @param[in] dt -timestep
+     * @param[in] rhs_func - Function to compute time derivatives, dy/dt = f(t, y)
+     * @param[inout] y - On entry, state at time t.  On exit, state at time t + dt
      */
     void step(double t, double dt, const RHSFunctionType& rhs_func, State& y);
 
@@ -28,10 +28,10 @@ class StepperLieRK2Midpoint {
     /**
      * @brief Given state y at time t, perform step and update y to time t + dt
      *
-     * @param t - Time at beginning of step
-     * @param dt -timestep
-     * @param rhs_func - Function to compute time derivatives, dy/dt = f(t, y)
-     * @param y - On entry, state at time t.  On exit, state at time t + dt
+     * @param[in] t - Time at beginning of step
+     * @param[in] dt -timestep
+     * @param[in] rhs_func - Function to compute time derivatives, dy/dt = f(t, y)
+     * @param[inout] y - On entry, state at time t.  On exit, state at time t + dt
      */
     void step(double t, double dt, const RHSFunctionType& rhs_func, State& y);
 
